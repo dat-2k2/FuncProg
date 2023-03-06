a48a8710475e8a91d7bb7756e53e57d27e50e4fd
dat-2k2 <socialnw.dat2002@gmail.com>
2023-03-06 19:17:10 +0300

    Assigment2 calculation finished

--- Assignment2/Assign2.hs
+++ Assignment2/Assign2.hs
@@ -4,6 +4,7 @@ module Main where
 import Language.Haskell.TH.PprLib (sep)
 import Distribution.PackageDescription (BuildType(Custom))
 import Data.Array (Ix(range))
+import Data.Semigroup (diff)
 
 valid:: [Int] -> [[Int]] -> Bool
 valid a [] = True
@@ -26,24 +27,33 @@ mergeCustom [] = []
 mergeCustom x = head x ++ mergeCustom (tail x)
 
 --print all paths from root to leaves
-allPaths :: CustomTree -> [[Int]]
+allPaths :: CustomTree ->  [[Int]]
 allPaths Nil = [[]]
-allPaths x = map (y ++) (mergeCustom (map allPaths (children x))) where y = if value x == 0 then [] else [value x]
+allPaths x = map ( ++ y) (mergeCustom (map allPaths (children x))) where y = if value x == 0 then [] else [value x]
 
 rangeCustom:: Int -> [Int]
 rangeCustom 0 = []
-rangeCustom n = n: rangeCustom (n-1)
+rangeCustom n =  n:rangeCustom (n-1)
 
 -- put one rook on each row
--- Recursive by size of available rows
-permutationwithfilter:: Int -> Int -> [Int]->CustomTree
-permutationwithfilter a b [] = CustomTree b [Nil]
+-- Recursive by size of available rows (AFTER PUT ROOK ON POS)
+permutation:: Int -> Int -> [Int] ->CustomTree
+permutation a b [] = CustomTree b [Nil]
 -- map with multiple-arg function
-permutationwithfilter 1 pos rows = CustomTree pos [Nil]
-permutationwithfilter n pos rows = CustomTree pos (map (\p -> permutationwithfilter (n-1) p (filter (\x -> x/= p && x/= pos) rows)) (filter (/=pos) rows))
+permutation 1 pos availrows = CustomTree pos [Nil]
+permutation n pos availrows = CustomTree pos (map (\p -> permutation (n-1) p (filter (\x -> x/= p && x/= pos) availrows)) (filter (/=pos) availrows))
 
+pwithfilter:: Int -> Int -> [Int] -> ([Int] -> Bool) -> CustomTree
+pwithfilter a b [] f = if not( f [b, a] ) then Nil else CustomTree b [Nil]
+pwithfilter 1 pos rows f = if not( f [pos, 1] ) then Nil else CustomTree pos [Nil]
+pwithfilter n pos rows f = if not( f [pos, n] ) then Nil else
+    CustomTree pos (map (\p -> pwithfilter (n-1) p (filter (\x -> x/= p && x/= pos) rows) f) (filter (/=pos) rows))
+    
 rooks:: Int -> CustomTree
-rooks n = permutationwithfilter (n+1) 0 (rangeCustom n)
+rooks n = permutation (n+1) 0  (rangeCustom n) 
+
+diffrooks:: Int -> CustomTree
+diffrooks n = pwithfilter (n+1) 0  (rangeCustom n) (\a -> not (a!!0 == a!!1))
 
 main::IO()
 main = do
@@ -51,6 +61,7 @@ main = do
     let leaf1 = CustomTree 1 [Nil]
     let leaf2 = CustomTree 2 [Nil]
     let root = CustomTree 1 [leaf1, leaf2]
-    print (allPaths (rooks 10))
+    let n = 4
+    print (filter(\x -> length x == n) (allPaths (diffrooks 7)))
 --ver2: diagonal is blocked. 
 
